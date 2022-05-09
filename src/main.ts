08d3489ece6fb89f966a990771521d9151aa3f34
user01 <caioviniciusdev@gmail.com>
2022-05-09 19:03:51 -0400

    feat: application start

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import './style.css'
+import Context from './controllers/Context';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+export default class Application {
+  context: Context;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  constructor() {
+    this.context = new Context();
+  }
+}
+
+const application = new Application();
